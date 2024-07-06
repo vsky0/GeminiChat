480030df7eb898e18d024cbf0ad419b4f3cc38bc
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-07-06 18:39:41 +0530

    fixed markdown issues

--- chatbot/chat/views.py
+++ chatbot/chat/views.py
@@ -4,6 +4,10 @@ from django.http import JsonResponse
 from django.contrib import auth
 from django.contrib.auth.models import User
 
+from . models import Chat
+
+from django.utils import timezone
+
 # for google generativeai
 import pathlib
 import textwrap
@@ -11,7 +15,8 @@ import textwrap
 import google.generativeai as genai
 
 from IPython.display import display
-from IPython.display import Markdown
+# from IPython.display import Markdown
+import markdown
 
 # For api key
 import os
@@ -21,18 +26,22 @@ load_dotenv()
 # accessing api key
 api_key = os.getenv('API_KEY')
 
+# configure api key
+genai.configure(api_key=api_key)
+
 # selecting the model 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Markdown response
 def to_markdown(text):
     text = text.replace('.', '  *')
-    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+    return markdown.markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 # Ask gemini
 def ask_gemini(message):
     response = model.generate_content(message)
     answer = to_markdown(response.text)
+
     return answer
     
 
@@ -41,6 +50,9 @@ def chat(request):
     if request.method == 'POST':
         message = request.POST.get('message')
         response = ask_gemini(message)
+
+        chat = Chat(user = request.user, message=message, response=response, created_at=timezone.now())
+        chat.save()
         return JsonResponse({'message':message, 'response':response})
     return render(request, 'chat.html')
 
